2ddaee28fba749cb29a09cf83486cec26c33b61b
lewis sneddon <lewis@pop-os.localdomain>
2023-04-29 22:04:55 +0100

    moved options to opts.lua

--- lua/opts.lua
+++ lua/opts.lua
@@ -18,3 +18,21 @@ set signcolumn=yes
 autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
 ]])
 
+vim.o.background = "dark"
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.swapfile = false
+
+vim.o.hlsearch = true
+vim.o.mouse = 'a'
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.updatetime = 250
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+--vim.o.completeopt = 'menuone,noselect'
+vim.o.termguicolors = true
